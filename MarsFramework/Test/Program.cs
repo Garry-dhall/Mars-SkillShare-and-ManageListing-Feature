d55c05bc3dea8f33c1510a30db0d202abfc3b998
Garry <gdhall100@gmail.com>
2021-02-25 23:23:45 +1300

    Mars SkillShare and ManageListings

---
+++ MarsFramework/Test/Program.cs
@@ -0,0 +1,30 @@
+﻿using MarsFramework.Pages;
+using NUnit.Framework;
+
+
+namespace MarsFramework
+{
+    public class Program
+    {
+        [TestFixture]
+        [Category("Sprint1")]
+        class User : Global.Base
+        {
+
+            [Test]
+            public void Test()
+            {
+                ShareSkill obj = new ShareSkill();
+                obj.EnterShareSkill();
+                //ManageListings Obj2 = new ManageListings();
+                 // Obj2.Viewlistings();
+                //Obj2.EditListing();
+                //Obj2.DeleteListing();
+            }
+
+
+
+        }
+    }
+}
+
